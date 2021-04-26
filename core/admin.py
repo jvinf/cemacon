807023b1aa5f32b90b66a59e661f8fcc35090c1f
jvinf <931528@gmail.com>
2021-04-26 17:26:57 -0300

    arquivos heroku

---
+++ core/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Cargo, Servico, Funcionario, Feature
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ('cargo', 'ativo', 'modificado')
+
+@admin.register(Servico)
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = ('servico', 'icone', 'ativo', 'modificado')
+
+@admin.register(Funcionario)
+class EquipeAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cargo', 'ativo', 'modificado')
+
+
+@admin.register(Feature)
+class FeatureAdmin(admin.ModelAdmin):
+    list_display = ('feature', 'icone')
+
