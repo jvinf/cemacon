f440646f3d4087cb764c119548fef1f7f8fa11c1
jvinf <931528@gmail.com>
2021-04-26 17:28:12 -0300

    arquivos heroku

--- fusion/settings.py
+++ fusion/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)34x%10ga+qa(ci7b3^@z0wo^vm4!1zh&u_!@r#a19e#we#c41'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
